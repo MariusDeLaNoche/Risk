693c34755065d7205dc95aa446f2b9f45e0a17bb
Alex <gsocia@gmx.fr>
2018-01-15 21:55:23 +0100

    beanRegion ok

--- src/com/risk/beans/RegionBean.java
+++ src/com/risk/beans/RegionBean.java
@@ -2,30 +2,45 @@ package com.risk.beans;
 
 import java.util.List;
 
+/**
+ * Bean d'une région
+ * 
+ * @author Alex
+ *
+ */
 public class RegionBean {
 	private String name;
 	private String bonus;
 	private List<RegionBean> adjacencies;
-	
+
 	/**
 	 * Constructeur region
+	 * 
 	 * @param aName
 	 * @param aBonus
 	 */
-	public RegionBean (String aName, String aBonus) {
+	public RegionBean(String aName, String aBonus) {
 		this.name = aName;
 		this.bonus = aBonus;
 	}
-	
+
 	// ------
 	// --- METHODES
 	// ------
-	public boolean addRegionAdjacency(RegionBean aRegion) {
-		boolean ok = false;
-		//if()
-		return ok;
-	}
 	
+	/**
+	 * Ajoute une region dans la liste des adjacences de la region courante
+	 * @param aRegion la region a ajouter
+	 * @return boolean, true si ajout OK, false si la region existait déjà dans la liste
+	 */
+	public boolean addRegionAdjacency(RegionBean aRegion) {
+		// Region deja dans la liste des adjacences?
+		boolean isAlreadyAdjacent = this.adjacencies.stream().filter(o -> o.getName().equals(aRegion.getName())).findFirst().isPresent();
+		if (!isAlreadyAdjacent)
+			this.adjacencies.add(aRegion);
+		return !isAlreadyAdjacent;
+	}
+
 	// ------
 	// --- ACCESSEURS
 	// ------
@@ -38,7 +53,8 @@ public class RegionBean {
 	}
 
 	/**
-	 * @param name the name to set
+	 * @param name
+	 *            the name to set
 	 */
 	public void setName(String name) {
 		this.name = name;
@@ -52,7 +68,8 @@ public class RegionBean {
 	}
 
 	/**
-	 * @param bonus the bonus to set
+	 * @param bonus
+	 *            the bonus to set
 	 */
 	public void setBonus(String bonus) {
 		this.bonus = bonus;
@@ -66,7 +83,8 @@ public class RegionBean {
 	}
 
 	/**
-	 * @param adjacencies the adjacencies to set
+	 * @param adjacencies
+	 *            the adjacencies to set
 	 */
 	public void setAdjacencies(List<RegionBean> adjacencies) {
 		this.adjacencies = adjacencies;
