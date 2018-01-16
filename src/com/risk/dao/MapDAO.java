135ce618f7698a38070b79d4a4887cd7ca58b141
Alex <gsocia@gmx.fr>
2018-01-16 22:14:51 +0100

    imports inutiles

--- src/com/risk/dao/MapDAO.java
+++ src/com/risk/dao/MapDAO.java
@@ -1,7 +1,6 @@
 package com.risk.dao;
 
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlValue;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
