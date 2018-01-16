78732de8ca2062d3503a6a299c5385933736443f
Giraud <Giraud@DESKTOP-0D53GSA>
2018-01-16 21:59:23 +0100

    ROOOOOOT

--- src/com/risk/dao/MapDAO.java
+++ src/com/risk/dao/MapDAO.java
@@ -2,7 +2,9 @@ package com.risk.dao;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlValue;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
 public class MapDAO {
 	private RegionsDAO regions;
 	private ZonesDAO zones;
