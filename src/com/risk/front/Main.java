1cce29b0d001385238472fdc0300eca8a447d5b2
Alex <gsocia@gmx.fr>
2018-01-17 21:39:30 +0100

    demande nb joueurs recup mode de jeu recup nom joueurs creation bean joueurs

--- src/com/risk/front/Main.java
+++ src/com/risk/front/Main.java
@@ -2,11 +2,9 @@ package com.risk.front;
 
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
-
 import com.risk.dao.MapDAO;
 import com.risk.dao.ModeDAO;
-import com.risk.dao.RegionDAO;
+import com.risk.metier.BeanCreator;
 import com.risk.metier.XmlReader;
 import com.risk.front.MainHelper;
 
@@ -16,26 +14,23 @@ import com.risk.front.MainHelper;
  *
  */
 public class Main {
-
-	static int nbJoueurs;
 	
 	public static void main(String[] args) {
 		// Art magnifique++
 		System.out.println("_-' RISK '-_\n");
 		System.out.println("Bienvenue!");
-		
+
 		XmlReader xml = new XmlReader();
 		MapDAO map = xml.unmarshalXML();
-		List<RegionDAO> lesRegions = map.getRegions().getListRegion();
+		BeanCreator beanCreator = new BeanCreator(map);
 		List<ModeDAO> lesModes = map.getModes().getModes();
+
+		// Demande du nombre de joueurs et récupération du mode de jeu correspondant
+		ModeDAO modeJeu = MainHelper.askNbPlayers(lesModes);
 		
-		
-		String demandeNbJoueurs = "Nouvelle partie : choisir un nombre de joueurs = ";		
-		do { nbJoueurs = MainHelper.getInputNumber(demandeNbJoueurs); }
-		while (lesModes.stream().noneMatch(o -> o.getPlayers() != nbJoueurs));
-		
-		
-		
+		// Création des beans des joueurs
+		beanCreator.setPlayers(
+				MainHelper.askPlayersNames(modeJeu.getPlayers()));
 		
 		
 		
--- src/com/risk/front/MainHelper.java
+++ src/com/risk/front/MainHelper.java
@@ -1,6 +1,11 @@
 package com.risk.front;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
+
+import com.risk.dao.ModeDAO;
 
 /**
  * 
@@ -8,6 +13,37 @@ import org.apache.commons.lang3.StringUtils;
  *
  */
 public class MainHelper {
+
+	static int nbJoueurs;
+	
+	/**
+	 * Retourne le mode de jeu correspondant au nombre de joueurs demandé par l'utilisateur
+	 * @return le mode de jeu choisit
+	 */
+	public static ModeDAO askNbPlayers(List<ModeDAO> lesModes) {
+		String demandeNbJoueurs = "Nouvelle partie : choisir un nombre de joueurs >= 2 : ";	
+		nbJoueurs = MainHelper.getInputNumber(demandeNbJoueurs);
+
+		while (lesModes.stream().noneMatch(o -> o.getPlayers() != nbJoueurs)) {
+			System.out.println("Aucun mode de jeu ne correspond à ce nombre de joueurs. Veuillez saisir un nouveau nombre : ");
+			nbJoueurs = MainHelper.getInputNumber(demandeNbJoueurs);
+		}
+		
+		return lesModes.stream().filter(o -> o.getPlayers() == nbJoueurs).findFirst().get();
+	}
+	
+	/**
+	 * Demande autant de noms de joueurs que spécifié en paramètre, et retourne la liste des noms
+	 * @param nbPlayers le nombre de joueurs
+	 * @return la liste des noms choisis par l'utilisateur
+	 */
+	public static List<String> askPlayersNames(int nbPlayers) {
+		List<String> listeNoms = new ArrayList<String>();
+		for (int i = 0; i < nbPlayers; i++) {
+			listeNoms.add(getInputString("Veuillez entrer le nom du joueur " + (i + 1) + " : "));
+		}
+		return listeNoms;
+	}
 	
 	/**
 	 * Permet de récuperer un entier saisi par l'utilisateur, avec une phrase personalisable
@@ -20,10 +56,28 @@ public class MainHelper {
 		
 		while(StringUtils.isNumeric(input)) {
 			System.out.println("Saisie invalide. Veuillez saisir un nombre!");
-			System.out.println("Nouvel essai = ");
+			System.out.println("Nouvel essai : ");
 			input = System.console().readLine();
 		}
 		
 		return Integer.parseInt(input);
+	}
+	
+	/**
+	 * Permet de récuperer une chaine de caracteres saisie par l'utilisateur, avec une phrase personalisable
+	 * @param text la phrase a afficher
+	 * @return la chaine entrée par l'utilisateur
+	 */
+	public static String getInputString(String text) {
+		System.out.println(text);
+		String input = System.console().readLine();
+		
+		while(input.length() == 0) {
+			System.out.println("Saisie invalide. Veuillez saisir une chaine de caractères!");
+			System.out.println("Nouvel essai : ");
+			input = System.console().readLine();
+		}
+		
+		return input;
 	}
 }
